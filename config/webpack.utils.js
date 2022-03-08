6e883660bf8d3ce5398dea00badc8e19fb94d3fc
19h47 <jeremylevron@19h47.fr>
2022-03-08 14:18:27 +0100

    :bug: Fix path

--- config/webpack.utils.js
+++ config/webpack.utils.js
@@ -1,7 +1,9 @@
 const path = require('path'); // eslint-disable-line import/no-extraneous-dependencies
 
+const root = path.resolve(process.cwd());
+
 const resolve = dir => {
-	return path.join(__dirname, '..', dir);
+	return path.join(root, '..', dir);
 };
 
 module.exports = resolve;
