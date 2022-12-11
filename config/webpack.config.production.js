eeb57a127c20aaa2f9bad46f051021ae68674e12
19h47 <jeremylevron@19h47.fr>
2022-12-11 11:27:11 +0100

    :arrow_up: Upgrade dependencies

--- config/webpack.config.production.js
+++ config/webpack.config.production.js
@@ -5,24 +5,24 @@
  * @author Jérémy Levron <jeremylevron@19h47.fr> (https://19h47.fr)
  */
 
-const glob = require('glob-all');
+const glob = require("glob-all");
 
 // Webpack Plugins
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
-const PurgecssPlugin = require('purgecss-webpack-plugin');
-const purgecssWordpress = require('purgecss-with-wordpress');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const purgecssWordpress = require("purgecss-with-wordpress");
 
 // Webpack Utils
-const { resolve, useTailwind, getStyleLoader } = require('./webpack.utils');
+const { resolve, useTailwind, getStyleLoader } = require("./webpack.utils");
 
 const plugins = [
 	new CleanWebpackPlugin({
-		cleanOnceBeforeBuildPatterns: [resolve('dist')],
+		cleanOnceBeforeBuildPatterns: [resolve("dist")],
 	}),
 	new MiniCssExtractPlugin({
-		filename: 'css/main.[chunkhash:8].css',
+		filename: "css/main.[chunkhash:8].css",
 		chunkFilename: "css/[id].[chunkhash:8].css",
 	}),
 	new CompressionPlugin(),
@@ -32,24 +32,32 @@ if (!useTailwind) {
 	plugins.push(
 		new PurgecssPlugin({
 			paths: glob.sync(
-				[resolve('inc/**/*'), resolve('includes/**/*'), resolve('src/scripts/**/*'), resolve('src/img/**/*'), resolve('views/**/*')],
+				[
+					resolve("app/**/*"), // Bedrock
+					resolve("inc/**/*"),
+					resolve("includes/**/*"),
+					resolve("src/scripts/**/*"),
+					resolve("src/img/**/*"),
+					resolve("views/**/*"),
+					resolve("ressources/views/**/*"), // Bedrock
+				],
 				{
 					nodir: true,
-				},
+				}
 			),
 			safelist: purgecssWordpress.safelist,
 			defaultExtractor: content => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
 		})
-	)
+	);
 }
 
 module.exports = {
-	mode: 'production',
+	mode: "production",
 	output: {
-		filename: 'js/[name].[chunkhash:8].js',
+		filename: "js/[name].[chunkhash:8].js",
 	},
 	module: {
 		rules: [getStyleLoader(false)],
 	},
-	plugins
+	plugins,
 };
