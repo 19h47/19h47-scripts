3aac4b5fdb03cff4c44169fb400a473374c65b09
19h47 <jeremylevron@19h47.fr>
2022-12-17 10:09:12 +0100

    :sparkles: Copy static

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -5,47 +5,50 @@
  * @author Jérémy Levron <jeremylevron@19h47.fr> (https://19h47.fr)
  */
 
-
 // Webpack plugins
-const ESLintPlugin = require('eslint-webpack-plugin');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
-const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
-const WebpackNotifierPlugin = require('webpack-notifier');
-const { VueLoaderPlugin } = require('vue-loader');
+const CopyPlugin = require("copy-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
+const WebpackNotifierPlugin = require("webpack-notifier");
+const { VueLoaderPlugin } = require("vue-loader");
 
 // Webpack Options
-const alias = require('./webpack.resolve.alias');
-const rules = require('./webpack.module.rules');
+const alias = require("./webpack.resolve.alias");
+const rules = require("./webpack.module.rules");
 
 // Webpack utils
-const { resolve } = require('./webpack.utils');
+const { resolve } = require("./webpack.utils");
 
 // Options
-const eslintOptions = require('../.eslintrc');
+const eslintOptions = require("../.eslintrc");
 
 module.exports = {
 	module: {
 		rules,
 	},
 	output: {
-		path: resolve('dist'),
+		path: resolve("dist"),
 	},
 	resolve: {
 		alias,
 	},
 	optimization: {
 		splitChunks: {
-			chunks: 'all',
-			name: 'vendors',
+			chunks: "all",
+			name: "vendors",
 		},
 	},
 	externals: {
-		jquery: 'jQuery',
-		$: 'jQuery',
+		jquery: "jQuery",
+		$: "jQuery",
 	},
 	plugins: [
+		new CopyPlugin({
+			patterns: [{ from: resolve("static") }],
+		}),
 		new WebpackManifestPlugin({
-			publicPath: 'dist/',
+			publicPath: "dist/",
 		}),
 		new ESLintPlugin({
 			baseConfig: eslintOptions,
@@ -53,7 +56,7 @@ module.exports = {
 		new VueLoaderPlugin(),
 		new SpriteLoaderPlugin({ plainSprite: true }),
 		new WebpackNotifierPlugin({
-			title: 'Webpack',
+			title: "Webpack",
 			excludeWarnings: true,
 			alwaysNotify: true,
 		}),
